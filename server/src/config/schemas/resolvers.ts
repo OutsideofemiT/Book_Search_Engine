697725b370098267d0293a952b03604b5cfd6167
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-04-03 23:10:09 -0500

    updated resolvers

--- server/src/config/schemas/resolvers.ts
+++ server/src/config/schemas/resolvers.ts
@@ -1,4 +1,4 @@
-import User from '../../models/User';
+import User from '../../models/User.js';
 import { signToken } from '../../services/auth';
 import { AuthenticationError } from 'apollo-server-express';
 
