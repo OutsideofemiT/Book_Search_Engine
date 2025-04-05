627605e3fd0684daea03552cf9adda73406cab61
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-04-04 21:29:16 -0500

    updated viteconfig, apolloclient to resolve deployment issues

--- client/src/utils/apolloClient.ts
+++ client/src/utils/apolloClient.ts
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql',
+  uri: process.env.REACT_APP_API_URL
+    ? `${process.env.REACT_APP_API_URL}/graphql`
+    : '/graphql',
   cache: new InMemoryCache(),
 });
 
