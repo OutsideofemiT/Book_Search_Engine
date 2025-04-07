0f51206a9bc2fff9a37d3e5182d026675f6f5cd2
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-04-07 13:23:59 -0500

    updated client/env

--- client/src/utils/apolloClient.ts
+++ client/src/utils/apolloClient.ts
@@ -1,9 +1,24 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: import.meta.env.VITE_API_URL
     ? `${import.meta.env.VITE_API_URL}/graphql`
     : '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
