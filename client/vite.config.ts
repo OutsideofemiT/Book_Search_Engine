d22d7a52dfaf18a431b928860b8228d95ce2ff33
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-04-18 14:44:16 -0500

    updated vite install

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build'
-  },
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/graphql': {
-        target: 'http://localhost:3001',
-        secure: false,
-        changeOrigin: true
-      }
-    }
-  }
 })
-
